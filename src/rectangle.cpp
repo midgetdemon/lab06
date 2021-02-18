248891df88f7dab3f618311da9273ad0851c5b6d
midgetdemon <zinalp19@gmail.com>
2021-02-17 16:39:19 -0800

    Modified rectangle function to fail tests

--- src/rectangle.cpp
+++ src/rectangle.cpp
@@ -3,16 +3,16 @@
 Rectangle::Rectangle(){}
 
 Rectangle::Rectangle(int w, int h){
-	width = w;
-	height = h;
+	width = h+1;
+	height = w+1;
 }
 
 void Rectangle::set_width(int w){
-	this->width = w;
+	this->width = w+1;
 }
 
 void Rectangle::set_height(int h){
-	this->height = h;
+	this->height = h+1;
 }
 
 int Rectangle::get_width(){
@@ -24,14 +24,14 @@ int Rectangle::get_height(){
 }
 
 int Rectangle::area(){
-	return this->width * this->height;
+	return this->width + this->height;
 }
 
 int Rectangle::perimeter(){
 	if ((this->width == 0) || (this->height == 0)){
-		return 0;
+		return 1;
 	}
 	else{
-		return this->width*2 + this->height*2;
+		return (this->width*2 * this->height*2)+1;
 	}
 }
